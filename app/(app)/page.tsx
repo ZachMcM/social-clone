4322e5e08df59d62f52453957e63efe1152e4961
Zach McMullen <zachmcmullen04@gmail.com>
2023-09-21 13:31:42 -0400

    [Fix] fixing initial data component

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,20 +1,11 @@
-import Feed from "@/components/feed/feed"
+import FeedServer from "@/components/feed/feed-server"
 import { FeedSkeleton } from "@/components/feed/feed-skeleton"
-import { ExtendedPost } from "@/types/prisma"
 import { Suspense } from "react"
 
-async function getInitialData(): Promise<ExtendedPost[]> {
-  const res = await fetch(`${process.env.URL}/api/feed?cursor=0`, { cache: 'no-store' })
-  const data = await res.json()
-  return data
-}
-
-export default async function Home() {
-  const initialPosts = await getInitialData()
-
+export default function Home() {
   return (
     <Suspense fallback={<FeedSkeleton/>}>
-      <Feed initialData={initialPosts}/>
+      <FeedServer/>
     </Suspense>
   )
 }
