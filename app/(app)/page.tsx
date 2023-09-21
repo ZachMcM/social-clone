6200f891e93cdc2995b8393ef4c12380a5846729
Zach McMullen <zachmcmullen04@gmail.com>
2023-09-21 11:36:26 -0400

    [Update] turning home page into server comp

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,79 +1,20 @@
-"use client";
+import Feed from "@/components/feed/feed"
+import { FeedSkeleton } from "@/components/feed/feed-skeleton"
+import { ExtendedPost } from "@/types/prisma"
+import { Suspense } from "react"
 
-import { useInfiniteQuery } from "react-query";
-import { useEffect, useRef } from "react";
-import { useIntersection } from "@mantine/hooks";
-import { FeedSkeleton } from "@/components/feed/feed-skeleton";
-import { ExtendedPost } from "@/types/prisma";
-import { FeedPost } from "@/components/feed/feed-post";
-import { Loader2 } from "lucide-react";
-import { toast } from "@/components/ui/use-toast";
-import { ToastAction } from "@/components/ui/toast";
-import { useRouter } from "next/navigation";
+async function getInitialData(): Promise<ExtendedPost[]> {
+  const res = await fetch(`${process.env.URL}/api/feed?cursor=0`, { cache: 'no-store' })
+  const data = await res.json()
+  return data
+}
 
-export default function Home() {
-  const router = useRouter()
-
-  const { data: posts, isLoading, fetchNextPage, isFetchingNextPage } =
-    useInfiniteQuery({
-      queryFn: async ({ pageParam = 0 }): Promise<ExtendedPost[]> => {
-        const res = await fetch("/api/feed?cursor=" + pageParam);
-        if (!res.ok) {
-          throw new Error("There was an error loading your feed.")
-        }
-        const data = await res.json();
-        console.log(data)
-        return data;
-      },
-      getNextPageParam: (_, pages) => {
-        return pages.length + 1;
-      },
-      onError: (err: Error) => {
-        console.log(err)
-        toast({
-          description: err.message,
-          variant: "destructive",
-          action: <ToastAction altText="try again" onClick={() => router.refresh()}>Try again</ToastAction>
-        })
-      },
-      queryKey: ['feed']
-    });
-
-  const lastPostRef = useRef<HTMLElement>(null);
-  const { ref, entry } = useIntersection({
-    root: lastPostRef.current,
-    threshold: 1,
-  });
-
-  useEffect(() => {
-    if (entry?.isIntersecting) {
-      console.log("getting next page")
-      fetchNextPage();
-    }
-  }, [entry, fetchNextPage]);
+export default async function Home() {
+  const initialPosts = await getInitialData()
 
   return (
-    <div className="flex-1 w-full h-full flex items-center flex-col gap-8">
-      {isLoading ? (
-        <FeedSkeleton />
-      ) : (
-        posts && posts.pages && (
-          <>
-            {posts.pages.flatMap((page) => page).map((post, i) => {
-              if (i === posts.pages.length - 1) {
-                return (
-                  <div ref={ref} key={post.id}>
-                    <FeedPost post={post} />
-                  </div>
-                );
-              } else {
-                return <FeedPost key={post.id} post={post} />;
-              }
-            })}
-            {isFetchingNextPage && <Loader2 className="h-4 w-4 animate-spin" />}
-          </>
-        )
-      )}
-    </div>
-  );
+    <Suspense fallback={<FeedSkeleton/>}>
+      <Feed initialData={initialPosts}/>
+    </Suspense>
+  )
 }
---
+++ components/feed/feed.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { useInfiniteQuery } from "react-query";
+import { useEffect, useRef } from "react";
+import { useIntersection } from "@mantine/hooks";
+import { FeedSkeleton } from "@/components/feed/feed-skeleton";
+import { ExtendedPost } from "@/types/prisma";
+import { FeedPost } from "@/components/feed/feed-post";
+import { Loader2 } from "lucide-react";
+import { toast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
+import { useRouter } from "next/navigation";
+
+export default function Feed({ initialData }: { initialData: ExtendedPost[] }) {
+  const router = useRouter();
+
+  const { data, isLoading, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ["feed"],
+      queryFn: async ({ pageParam = 1 }) => {
+        const res = await fetch("/api/feed?cursor=" + pageParam);
+        if (!res.ok) {
+          throw new Error("There was an error loading your feed.");
+        }
+        const data = await res.json();
+        console.log(data);
+        return data;
+      },
+      getNextPageParam: (_, pages) => {
+        return pages.length + 1;
+      },
+      initialData: {
+        pages: [initialData],
+        pageParams: [1],
+      },
+      onError: (err: Error) => {
+        console.log(err);
+        toast({
+          description: err.message,
+          variant: "destructive",
+          action: (
+            <ToastAction altText="try again" onClick={() => router.refresh()}>
+              Try again
+            </ToastAction>
+          ),
+        });
+      },
+    });
+
+  const lastPostRef = useRef<HTMLElement>(null);
+  const { ref, entry } = useIntersection({
+    root: lastPostRef.current,
+    threshold: 1,
+  });
+
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      console.log("getting next page");
+      fetchNextPage();
+    }
+  }, [entry, fetchNextPage]);
+
+  const posts: ExtendedPost[] =
+    data?.pages.flatMap((page) => page) ?? initialData;
+
+  return (
+    <div className="flex-1 w-full h-full flex items-center flex-col gap-8">
+      {posts.map((post, i) => {
+        if (i === posts.length - 1) {
+          return (
+            <div ref={ref} key={post.id}>
+              <FeedPost post={post} />
+            </div>
+          );
+        } else {
+          return <FeedPost key={post.id} post={post} />;
+        }
+      })}
+      {isFetchingNextPage && <Loader2 className="h-4 w-4 animate-spin" />}
+    </div>
+  );
+}
