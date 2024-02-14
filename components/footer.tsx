781d6483b187ca95781790c343aebc46f0cd63c2
Zach McMullen <zachmcmullen04@gmail.com>
2024-02-13 21:31:42 -0500

    [ui] footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,10 @@
 export function Footer() {
   return (
-    <div className="w-full gap-4 flex items-center border-t p-4">
+    <div className="w-full gap-4 flex items-center border-t p-8">
       <p className="font-medium text-xs">
         Built by{" "}
         <a className="underline" href="https://zachmcmullen.com">
           Zach McMullen
-        </a>
-        . Hosted on{" "}
-        <a className="underline" href="https://vercel.com">
-          Vercel
         </a>
         . Source code available on{" "}
         <a className="underline" href="https://github.com/ZachMcM/social-clone">
